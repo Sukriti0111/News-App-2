f58b2e9698d83d3cbe73a41371ad65ac073788a6
Sukriti0111 <ksukriti911@gmail.com>
2023-05-30 17:44:14 +0530

    First commit

---
+++ src/components/home/Home.js
@@ -0,0 +1,100 @@
+import React, { useEffect, useState } from "react";
+import NewsCard from "../newscard/NewsCard";
+import "./Home.css";
+import axios from "../axiosClient/axiosClient";
+
+function Home() {
+  const apiKey = "ce1e6693338d4eb0a089fe2ca839eeda";
+
+  const [myData, setmyData] = useState([]);
+  const [searchQ, setsearchQ] = useState("");
+
+  async function fetchData(key) {
+    try {
+      const data = await axios.get(`/top-headlines?country=in&apiKey=${key}`);
+      setmyData(data.data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  async function searchDataFetch(key, search) {
+    try {
+      const data = await axios.get(`/everything?q=${search}&apiKey=${key}`);
+      setmyData(data.data.articles);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(apiKey);
+  }, []);
+
+  return (
+    <div className="Home ">
+      <div className="container ">
+        <nav className="flex">
+          <div className="heading flex">
+            <h1 className="title">SUK NEWS</h1>
+            <ul className=" list flex">
+              <li
+                onClick={() => {
+                  searchDataFetch(apiKey, "sports");
+                }}
+              >
+                Sports
+              </li>
+              <li
+                onClick={() => {
+                  searchDataFetch(apiKey, "entertainment");
+                }}
+              >
+                Entertainment
+              </li>
+              <li
+                onClick={() => {
+                  searchDataFetch(apiKey, "business");
+                }}
+              >
+                Politics
+              </li>
+            </ul>
+          </div>
+          <div className="search-box flex">
+            <input
+              type="text"
+              placeholder="Search News"
+              onChange={(e) => {
+                setsearchQ(e.target.value);
+              }}
+            />
+
+            <button
+              onClick={() => {
+                searchDataFetch(apiKey, searchQ);
+              }}
+            >
+              Search
+            </button>
+          </div>
+        </nav>
+        <div className="news-feed flex">
+          {myData.map((item) => {
+            return (
+              <NewsCard
+                title={item.title}
+                urlToImage={item.urlToImage}
+                url={item.url}
+                description={item.description}
+                publishedAt={item.publishedAt}
+                key={item.id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/components/newscard/NewsCard.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./NewsCard.css";
+import moment from "moment";
+
+function NewsCard({ title, urlToImage, url, description, publishedAt, key }) {
+  const date = moment(publishedAt).utc().format("DD-MM-YYYY");
+  return (
+    <div className="NewsCard center">
+      <div className="container">
+        <div className="image">
+          <img src={urlToImage} alt="" />
+        </div>
+        <h4>{title}</h4>
+        <p className="desc">{description}</p>
+        <p className="date">Publish at: {date}</p>
+        <span>
+          <a href={url}>
+            <button>Read More...</button>
+          </a>
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default NewsCard;
